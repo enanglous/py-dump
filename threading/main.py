218035d0a9064ebc4e73b02e995adc67342d5eaf
enanglous <idiswaddis@gmail.com>
2025-07-03 23:00:17 +0500

    fix linting

--- threading/main.py
+++ threading/main.py
@@ -1,10 +1,11 @@
 import threading
-import time
 from concurrent.futures import ThreadPoolExecutor
+
 
 def threaded_function():
     for number in range(3):
         print(f"Printing from {threading.current_thread().name}. {number=}")
+
 
 with ThreadPoolExecutor(max_workers=10, thread_name_prefix="Worker") as executor:
     for _ in range(10):
